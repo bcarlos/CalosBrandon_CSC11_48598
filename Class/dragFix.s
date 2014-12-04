b72d1d2cd3e7716fa7f50d6ef09fa0ed413c4d39
brandon <bc2268896@CSC11.(none)>
2014-12-04 13:25:51 -0800

    a

--- Class/dragFix.s
+++ Class/dragFix.s
@@ -3,6 +3,7 @@ msg1: .asciz " Integer dynamic pressure = %f lbs \n "
 msg2: .asciz " Cross sectional area x 32 = %f ft^2 \n"
 msg3: .asciz " Integer drag x 32 %f lbs \n"
 msg4: .asciz " Loops : %d \n" @ loops is digit not float
+fmt_flt: .asciz "%f"
 
 value0: .float 0.00237 @ fRho
 value1: .float 3.14159 @ fPi
@@ -22,25 +23,25 @@ main:
 push {r4, lr} @ align with 8 bytes
 sub sp, sp, #8 @ make room for one 8 byte integer in the stack
 
-mov r0, #0 @ declare registers
-mov r1, #0 @ declare registers
-mov r2, #0 @ declare registers
 ldr r3, =10000000 @ set loop register to 10,000,000
- 
-
+ldr r0, ad_fmt_flt	@ load float format into r0
 /* First calculation fHalf, fRho, fVel into fDynp (d0) */
 ldr  r0, addr_value5 @ fHalf
 vldr s0, [r0] @ load into s0
-ldr  r1, addr_value0 @ fRho
-vldr s1, [r1] @ load into s1
-ldr  r2, addr_value6 @ fVel
-vldr s2, [r2] @ load into s2
+@ldr  r1, addr_value0 @ fRho
+@vldr s1, [r1] @ load into s1
+@ldr  r2, addr_value6 @ fVel
+@vldr s2, [r2] @ load into s2
  
 /* Remove from the stack and return */
         add sp, sp, #8       @ Discard the integer read by scanf
         pop {r4,lr}
         bx lr
-		
+
+.global printf		
+
+ad_fmt_flt: .word fmt_flt
+
 addr_value0: .word value0
 addr_value1: .word value1
 addr_value2: .word value2
--- Hmwk/dragInt.s
+++ Hmwk/dragInt.s
@@ -2,9 +2,8 @@
 
 message1: .asciz " Integer dynamic pressure = %d lbs \n "
 message2: .asciz " Cross sectional area x 32 = %d ft^2 \n"
-message3: .asciz " Integer drag x 32 %d lbs \n"
-message4: .asciz " Loops : %d \n"
-message5: .asciz " Time = %d secs \n"
+message3: .asciz " Integer drag x 32 = %d lbs \n"
+message4: .asciz " Loops : 10000000 \n"
 
 word1: .word 0x9B5		@ iRho
 word2: .word 0x3243F7		@ iPi
@@ -27,45 +26,38 @@ mov r6, #0 			@ empty reg
 
 @ bl start_clock
 
-@ mov r0, #10			@ nLoops
-@ loop:
-@  push {r4, r5, r6, lr}
-@  sub sp, sp, #4
-mov r3, #1
-ldr r6, address_of_word1	@ iRho
-ldr r6, [r6]
-mul r3, r6, r3 			@ iRho * r3 (iDynp)
-mov r6, #200	 		@ iVel = 200
-mul r3, r6, r3 			@ iVel * r3
-mul r3, r6, r3 			@ iVel * r3
-mov r3, r3, lsr #12 		@ shift right by 12
-
-ldr r4, address_of_word2	@ iArea = iPi
-ldr r4, [r4]
-mov r6, #6			@ iRad
-mul r4, r6, r4 			@ iRad * r4 (iArea)
-mul r4, r6, r4 			@ iRad * r4 (iArea)
-mov r4, r4, lsr #12 		@ shift right by 12
-
-ldr r6, address_of_word3	@ iConv
-ldr r6, [r6]
-mul r4, r6, r4 			@ r4 * iConv
-mov r4, r4, lsr #16 		@ shift right by 16
-mul r5, r4, r3 			@ iDrag = iArea * iDynp
-mov r5, r5, lsr #12 		@ shift right by 12
-
-ldr r6, address_of_word4	@ iCd
-ldr r6, [r6]
-mul r5, r6, r5 			@ iCd * iDrag
-
-@ sub r0, r0, #1
-@ cmp r0, #0
-@ bgt loop
-@  add sp, sp, #4
-@  pop {r4, r5, r6, lr}
-
-@ bl end_clock
-@ str r1, [r1]			@ store end time in r1
+ldr r0, =10000000		@ nLoops set to 10,000,000
+	loop:
+	mov r3, #1
+	ldr r6, address_of_word1	@ iRho
+	ldr r6, [r6]
+	mul r3, r6, r3 			@ iRho * r3 (iDynp)
+	mov r6, #200	 		@ iVel = 200
+	mul r3, r6, r3 			@ iVel * r3
+	mul r3, r6, r3 			@ iVel * r3
+	mov r3, r3, lsr #12 		@ shift right by 12
+	
+	ldr r4, address_of_word2	@ iArea = iPi
+	ldr r4, [r4]
+	mov r6, #6			@ iRad
+	mul r4, r6, r4 			@ iRad * r4 (iArea)
+	mul r4, r6, r4 			@ iRad * r4 (iArea)
+	mov r4, r4, lsr #12 		@ shift right by 12
+	
+	ldr r6, address_of_word3	@ iConv
+	ldr r6, [r6]
+	mul r4, r6, r4 			@ r4 * iConv
+	mov r4, r4, lsr #16 		@ shift right by 16
+	mul r5, r4, r3 			@ iDrag = iArea * iDynp
+	mov r5, r5, lsr #12 		@ shift right by 12
+	
+	ldr r6, address_of_word4	@ iCd
+	ldr r6, [r6]
+	mul r5, r6, r5 			@ iCd * iDrag
+	
+	sub r0, r0, #1
+	cmp r0, #0
+	bgt loop
 
 ldr r0, address_of_message1 	@ display iDynp
 mov r1, r3
@@ -79,13 +71,8 @@ ldr r0, address_of_message3 	@ display iDrag
 mov r1, r5
 bl printf
 
-@ ldr r0, address_of_message4 	@ display nLoops
-@ mov r1, r0
-@ bl printf
-
-@ ldr r0, address_of_message5 	@ display time
-@ ldr r1, [r1]			@r1 has endTime - begTime  
-@ bl printf
+ldr r0, address_of_message4 	@ display nLoops
+bl printf
 
 pop {r4, r5, r6, lr}
 bx lr
@@ -94,7 +81,6 @@ address_of_message1: .word message1
 address_of_message2: .word message2
 address_of_message3: .word message3
 address_of_message4: .word message4
-address_of_message5: .word message5
 
 address_of_word1: .word word1
 address_of_word2: .word word2
