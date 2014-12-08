2c1f07c37c625837e983742ecf4c5af995139f0f
bcarlos <bcarlos@student.rcc.edu>
2014-12-08 15:37:01 -0800

    Signed-off-by: bcarlos <bcarlos@student.rcc.edu>

--- Lab/float_array.s
+++ Lab/float_array.s
@@ -1,4 +1,5 @@
 .data
+
 .align 4
 Farray: .skip 148 		@ skip 36*4 bytes and +4 bytes
 
@@ -6,7 +7,9 @@ Farray: .skip 148 		@ skip 36*4 bytes and +4 bytes
 Carray: .skip 148		@ skip 36*4 bytes and +4 bytes
 
 .align 4
-msg: .asciz "Temperature %d F equals to :%d C\n"
+msg: .asciz "Temperature %d F equals to :%f C\n"
+@ first param should be int and second should be float
+
 .text
 
 F_array:
@@ -29,17 +32,31 @@ check_loop_Farray:
 
 C_array:
 /* r0: # of items;
-    r1: address of the array */
+    r1: address of the Carray, r3: address of Farray */
         push {r3, r4, r6, r7, lr}
         mov r4, #0
-		ldr r7, =0x8e38f
+@		ldr r7, =0x8e38f
         b check_loop_Carray
 loop_Carray:
-        ldr r6, [r3, r4, lsl #2]
-		sub r6, r6, #32
-		mul r6, r6, r7
-		mov r6, r6, lsr #20
-		str r6, [r1, r4, lsl #2]
+        ldr r6, [r3, r4, lsl #2]	@ r3 has Farray
+		@ perform calculation of temp
+		vmov s0, r6					@ float conversion of num in array
+		vcvt.f32.s32 s1, s0			@ convert to single precision f
+		mov r2, #32					@ prepare sub calculation
+		vmov s2, r2           		@ mov r2 into s2 to convert and sub
+		vcvt.f32.s32 s3, s2   		@ convert from single to float
+		vsub.f32 s1, s1, s3   		@ subtract into s1=s1(positiont at r4)-s3
+		mov r2, #5            		@ prepare to vmul where r2 = 5
+		vmov s2, r2           		@ mov r2 into s2 to convert
+		vcvt.f32.s32 s3, s2   		@ convert from single to float
+		vmul.f32 s1, s1, s3   		@ mul into s1=s1*s3
+		mov r2, #9            		@ prepare to calculate where r2 = 9
+		vmov s2, r2           		@ mov r2 into s2 to convert and divide
+		vcvt.f32.s32 s3, s2   		@ convert to float
+		vdiv.f32 s1, s1, s3   		@ div into s1=s1/s3
+        vmov r6, s1
+	
+		str r6, [r1, r4, lsl #2]	@ r1 has Carray
         add r4, r4, #1
 check_loop_Carray:
         cmp r4, r0
@@ -67,6 +84,7 @@ check_loop_print_array:
         bx lr
 
 .global main
+.func main
 main:
 	push {r4, lr}
 
