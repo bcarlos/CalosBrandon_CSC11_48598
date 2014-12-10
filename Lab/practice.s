b29b1048c0d5a9708a1d1bb6137082b6279fe1b8
bcarlos <bcarlos@student.rcc.edu>
2014-12-09 18:10:02 -0800

    Signed-off-by: bcarlos <bcarlos@student.rcc.edu>

--- Lab/practice.s
+++ Lab/practice.s
@@ -8,14 +8,24 @@ num: .float 0
 main:
 
 push {r4, lr}
-
-mov r1, #10
-vmov s1, r1
-mov r1, #5
-vmov s2, r1
-vsub.f32 s1, s1, s2
+ mov r0, #32
+loop:
+vmov s0, r0
+		vcvt.f32.s32 s1, s0			@ convert to single precision f
+		mov r2, #32					@ prepare sub calculation
+		vmov s2, r2           		@ mov r2 into s2 to convert and sub
+		vcvt.f32.s32 s3, s2   		@ convert from single to float
+		vsub.f32 s1, s1, s3   		@ subtract into s1=s1(positiont at r4)-s3
+		mov r2, #5            		@ prepare to vmul where r2 = 5
+		vmov s2, r2           		@ mov r2 into s2 to convert
+		vcvt.f32.s32 s3, s2   		@ convert from single to float
+		vmul.f32 s1, s1, s3   		@ mul into s1=s1*s3
+		mov r2, #9            		@ prepare to calculate where r2 = 9
+		vmov s2, r2           		@ mov r2 into s2 to convert and divide
+		vcvt.f32.s32 s3, s2   		@ convert to float
+		vdiv.f32 s1, s1, s3   		@ div into s1=s1/s3
 vcvt.f64.f32 d1, s1
-ldr r0, ad_msg_dyn_out
+ldr r0, =msg_dyn_out
 vmov r2, r3, d1
 bl printf
 
